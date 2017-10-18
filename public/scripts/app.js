018f36e4be9da07aa7489e39847287f9c6089e8e
eddycheong <cheong.e49@gmail.com>
2017-10-18 21:34:31 +0000

    Implementation of the toggle for compose

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -115,6 +115,14 @@ $(function() {
     });
   }
 
+  $("#nav-bar button").on("click", function(event) {
+    event.preventDefault();
+
+    $(".new-tweet").slideToggle(function() {
+      $(this).find("textarea").focus();
+    });
+  });
+
   loadTweets();
   submitTweet();
 });
